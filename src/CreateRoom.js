0ad723c0d6757fc3dee8a8cdfd81aac54ffde6e1
Simi <simona.priya.s@gmail.com>
2018-05-24 19:13:28 +0100

    store form input in variables

--- src/CreateRoom.js
+++ src/CreateRoom.js
@@ -1,15 +1,59 @@
 import React, { Component } from "react";
 
 class CreateRoom extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            room_name: "",
+            room_password: ""
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(event) {
+        alert('Room name: ' + this.state.room_name + '\nRoom password: ' + this.state.room_password);
+        event.preventDefault();
+    }
+
+
     render() {
         return (
             <div>
                 <p>Create room</p>
                 <p>Room name<br/>Password y/n<br/>Password<br/>Visible or search-only</p>
-                <form>
-                    Enter room name:<input type="text" id="room_name"/><br/>
-                    Enter password (leave blank for no pw):<input type="text" id="room_password"/><br/>
-                    <input type="button" value="Create room"/>
+
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                    Enter room name:
+                    <input
+                        name="room_name"
+                        type="text"
+                        value={this.state.room_name}
+                        onChange={this.handleInputChange}/><br/>
+                    </label>
+
+                    <label>
+                    Enter password (leave blank for no pw):
+                    <input
+                        name="room_password"
+                        type="text"
+                        value={this.state.room_password}
+                        onChange={this.handleInputChange}/><br/>
+                    </label>
+                    <input type="submit" value="Create room"/>
                 </form>
             </div>
         );
