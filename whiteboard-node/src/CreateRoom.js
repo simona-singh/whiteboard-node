10514e7bbf06d0e735d0bd9aec74311d32e5155b
Simi <simona.priya.s@gmail.com>
2018-05-24 13:58:29 +0100

    fix directories

--- whiteboard-node/src/CreateRoom.js
+++ whiteboard-node/src/CreateRoom.js
@@ -6,7 +6,11 @@ class CreateRoom extends Component {
             <div>
                 <p>Create room</p>
                 <p>Room name<br/>Password y/n<br/>Password<br/>Visible or search-only</p>
-
+                <form>
+                    Enter room name:<input type="text" id="room_name"/><br/>
+                    Enter password (leave blank for no pw):<input type="text" id="room_password"/><br/>
+                    <input type="button" value="Create room"/>
+                </form>
             </div>
         );
     }
